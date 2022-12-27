648ea523015c77a771b5e740b4ed800e6c7b0089
alonzojp <alonzojp@gmail.com>
2022-12-27 03:13:59 -0800

    Created a working tag check for a single combination

--- src/main/java/com/example/akrecruitmentscanner/HelloController.java
+++ src/main/java/com/example/akrecruitmentscanner/HelloController.java
@@ -63,36 +63,26 @@ public class HelloController {
 
         ImageIO.write(img,"png", processor);
 
-//        int width = croppedImage.getWidth();
-//        int height = croppedImage.getHeight();
-//
-//        for (int y = 0; y < height; y++) {
-//            for (int x = 0; x < width; x++) {
-//
-//                // Here (x,y)denotes the coordinate of image
-//                // for modifying the pixel value.
-//                int p = image.getRGB(x, y);
-//
-//                int a = (p >> 24) & 0xff;
-//                int r = (p >> 16) & 0xff;
-//                int g = (p >> 8) & 0xff;
-//                int b = p & 0xff;
-//
-//                // calculate average
-//                int avg = (r + g + b) / 3;
-//
-//                // replace RGB value with avg
-//                p = (a << 24) | (avg << 16) | (avg << 8)
-//                        | avg;
-//
-//                image.setRGB(x, y, p);
-//            }
-//        }
-
         try {
             tr.setDatapath("src/main/resources/com.example.tesseractfiles/TesseractFiles/Tess4J");
-            String text = tr.doOCR(processor);
-            System.out.println(text);
+            String scannedText = tr.doOCR(processor);
+
+            if(scannedText.contains("Fast")) {
+                System.out.println("4* Found!");
+                System.out.println(scannedText);
+            }
+            else {
+                System.out.println("No good combinations found.");
+                System.out.println(scannedText);
+            }
+
+//            String[] processedText = scannedText.split(" ");
+//
+//            for(int i = 0; i < processedText.length; i++) {
+//                if(processedText[i].length() > 3) {
+//                    System.out.println(processedText[i]);
+//                }
+//            }
         }
         catch(TesseractException e) {
             System.out.println(e.toString());
